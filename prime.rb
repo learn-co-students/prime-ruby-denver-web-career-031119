7ee246ce3c75d4814a030c53c75b2328556828a6
KendallWillard <KendallWillard@hotmail.com>
2019-02-19 17:09:10 -0600

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
 # Add  code here!
+require 'pry'
+
+def prime?(num)
+  if num <= 1
+    return false
+  end
+
+  (2..num - 1).each do |number|
+    return false if num % number == 0
+  end
+  return true
+end

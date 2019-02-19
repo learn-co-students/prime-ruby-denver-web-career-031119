36cd17eead33a2d514d696aa8c39d0222c3ae808
KendallWillard <KendallWillard@hotmail.com>
2019-02-19 17:10:28 -0600

    Done.

--- prime.rb
+++ prime.rb
@@ -5,7 +5,8 @@ def prime?(num)
   if num <= 1
     return false
   end
-
+  # array of possible numbers that could be divisible by the num
+  # that would disqualify the num to be prime
   (2..num - 1).each do |number|
     return false if num % number == 0
   end

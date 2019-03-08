dd2a6fecd722951abc4afd3bb9ee13ef67965361
github username <github email address>
2019-03-07 21:26:32 -0700

    Done.

--- prime.rb
+++ prime.rb
@@ -1,6 +1,15 @@
 def prime?(num)
-    if num #is prime
-        true
-    else
-        false
+    if num < 0 or num == 0 or num == 1
+        return false
+        else
+        (2..num-1).to_a.all? do |possible_factor|
+            num % possible_factor != 0
+        end
+    end
+end
 
+puts prime?(105557)
+
+
+
+#REVIEW THIS!?
